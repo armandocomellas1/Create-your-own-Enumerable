21df066a4345ea7b4ba7203179ade4b48b35282f
GameMaster <businesswitharmando@gmail.com>
2022-10-27 20:48:22 -0500

    Added the three methods to the module file

--- MyEnumerable.rb
+++ MyEnumerable.rb
@@ -1,2 +1,16 @@
 module MyEnumerable
+  def self.all(props)
+    props.all? {|e| e < 5}
+    props.all? {|e| e > 5}
+  end
+
+  def self.any(props)
+    props.any? {|e| e == 2}
+    props.any? {|e| e == 5}
+  end
+
+  def self.filter(props)
+    props.any? {|e| e == 2}
+    props.any? {|e| e == 5}
+  end
 end
--- MyList.rb
+++ MyList.rb
@@ -1,17 +1,36 @@
 class MyList
-  include Enumerable
-  def initialize(*x)
-    @list = x
-  end
-
-  #now we implement #each
-  def each(&block)
-    @list.each(&block)
-    self #return the original array
-  end
-
   require_relative 'MyEnumerable'
+  extend MyEnumerable
+  def initialize(*args)
+    @list = Array.new(args)
+  end
+
+  def each
+    yield(@list)
+    MyEnumerable::all(@list)
+    MyEnumerable::any(@list)
+    MyEnumerable::filter(@list)
+  end
 end
 
-list = MyList.new(1, 2, 3, 4)
+list = MyList.new(1,2,3,4)
 p list
+
+
+@example = 0
+def pick_example(list)
+  list.each do |num|
+    @example = num
+  end
+end
+pick_example(list)
+print @example
+list = @example
+
+p(list.all? {|e| e < 5})
+p(list.all? {|e| e > 5})
+
+p(list.any? {|e| e == 2})
+p(list.any? {|e| e == 5})
+
+p(list.filter {|e| e.even?})
