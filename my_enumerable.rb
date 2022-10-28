410503eb8d45e9b9d697b36b6021edee01ae20f4
angeluray <aeucampos@gmail.com>
2022-10-28 09:53:31 -0400

    Fix linters from my_enumerable and my_list.rb

--- my_enumerable.rb
+++ my_enumerable.rb
@@ -3,27 +3,24 @@ module MyEnumerable
   # @list is the instance variable of the class that includes this module
   # yeild is the block that is passed to the method so yield(e) is the same as block.call(e)
   def all?
-    @list.each {
-      |e| # e is the condition to be checked
+    @list.each do |e| # e is the condition to be checked
       return false unless yield(e) # if the condition is false, return false
-    }
-    true    # if all elements pass the condition
+    end
+    true # if all elements pass the condition
   end
 
   def any?
-    @list.each {
-      |e| # e is the condition to be checked
+    @list.each do |e| # e is the condition to be checked
       return true if yield(e) # if the condition is true, return true
-    }
-    false    # if all elements pass the condition
+    end
+    false # if all elements pass the condition
   end
 
   def filter
     filtered_list = []
-    @list.each {
-      |e| # e is the condition to be checked
+    @list.each do |e| # e is the condition to be checked
       filtered_list << e if yield(e) # if the condition is true, push the element to the filtered list
-    }
+    end
     filtered_list
   end
 end
--- my_list.rb
+++ my_list.rb
@@ -1,5 +1,5 @@
-require_relative "my_enumerable"
-#class with a enumerable methods
+require_relative 'my_enumerable'
+# class with a enumerable methods
 
 class MyList
   include MyEnumerable
@@ -15,9 +15,8 @@ end
 
 list = MyList.new(1, 2, 3, 4)
 
-
-puts(list.all? {|e| e < 5})
-puts(list.all? {|e| e > 5})
-puts(list.any? {|e| e == 2})
-puts(list.any? {|e| e == 5})
-puts(list.filter {|e| e.even?})
+puts(list.all? { |e| e < 5 })
+puts(list.all? { |e| e > 5 })
+puts(list.any? { |e| e == 2 })
+puts(list.any? { |e| e == 5 })
+puts(list.filter { |e| e.even? })
