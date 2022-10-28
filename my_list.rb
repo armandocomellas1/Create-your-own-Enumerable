059087f6c30dc7ad72e5d92174536c614729178d
Mov305 <abd3052001@gmail.com>
2022-10-28 11:09:14 +0200

    added the solution

--- my_list.rb
+++ my_list.rb
@@ -1,32 +1,23 @@
-require_relative 'my_enumerable'
-
-# Using the include inside our MyList class will allow MyEnumerable to
-# have access to the list with parameters.
+require_relative "my_enumerable"
+#class with a enumerable methods
 
 class MyList
   include MyEnumerable
-  # Here we initialize a variable using the splat operator
-  # Type an (*) before the name of the variable means that we'll pass many argmnst
-  # parameters are collected up and an array is created.
-  def initialize(*my_list)
-    @list = my_list
+
+  def initialize(*items)
+    @list = items
   end
 
-  # Here we create an explicit block, which basically calls or retrieve the items
-  # every time we use an enumerable
-  def each(&block)
-    @list.each(&block)
-
-    # It's the same that the code below
-    # @list.each do |item|
-    # block.call(item)
+  def each
+    yield(@list)
   end
 end
 
-mynewlist = MyList.new(1, 2, 3, 4, 5)
+list = MyList.new(1, 2, 3, 4)
 
-puts(mynewlist.all? {|e| e < 5})
-puts(mynewlist.all? {|e| e > 5})
-puts(mynewlist.any? {|e| e == 2})
-puts(mynewlist.any? {|e| e == 5})
-puts(mynewlist.filter {|e| e.even?})
+
+puts(list.all? {|e| e < 5})
+puts(list.all? {|e| e > 5})
+puts(list.any? {|e| e == 2})
+puts(list.any? {|e| e == 5})
+puts(list.filter {|e| e.even?})
